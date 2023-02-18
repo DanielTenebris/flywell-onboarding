1b00e18fd471473927850b97dae5fbb402b8ebbb
Ed Khalturin 'DVegasa <dracveg@gmail.com>
2023-02-19 01:37:28 +0300

    Конструктор роадмапов WIP

--- frontend/src/features/desktop/roadmaps/new/PageDesktopNewRoadmap.vue
+++ frontend/src/features/desktop/roadmaps/new/PageDesktopNewRoadmap.vue
@@ -2,21 +2,26 @@
   <div class="_dp" id="pageDesktopNewRoadmap">
     <h1>Новый роадмап обучения</h1>
 
+
     <h2 style="margin-top: 10px !important;">Придумайте имя курса обучения:</h2>
     <input class="appInput" type="text" placeholder="Название роадмапа"/>
 
+
     <h2>Роадмап будет содержать такие задания:</h2>
     <i v-if="selectedLessons.length === 0">(пусто)</i>
-
-    <div class="lesson" v-for="(lesson, i) in selectedLessons">
-      №{{i + 1}}: {{lesson.title}}
+    <div class="lessons">
+      <div class="lesson" v-for="(lesson, i) in selectedLessons">
+        <span>№{{i + 1}}: {{lesson.title}}</span>
+        <button class="appButton orange smallButton" @click="remove(i)">Убрать</button>
+      </div>
     </div>
 
+
     <h2>Добавьте ещё задание, с помощью этой формы</h2>
-    <div class="newLessons">
-      <div class="newLesson" v-for="newLesson in newLessons">
+    <div class="lessons">
+      <div class="lesson" v-for="newLesson in newLessons">
+        <button class="appButton lime btnAddLesson smallButton" @click="addLesson(newLesson)">Добавить</button>
         {{newLesson.title}}
-        <button class="appButton lime btnAddLesson" @click="addLesson">Добавить задание</button>
       </div>
     </div>
 
@@ -30,15 +35,22 @@
 import {ref} from "vue";
 import {repoRoadmap} from "../../../repo/repoRoadmap.js";
 import {repoMaterial} from "@/features/repo/repoMaterial.js";
+import {repoLesson} from "@/features/repo/repoLesson.js";
 
 const x = repoRoadmap().getAll();
 
 const selectedLessons = ref([]);
 
-const newLessons = ref([]);
+const newLessons = ref(repoLesson().getAll());
 
 const addLesson = (lesson) => {
   selectedLessons.value.push(lesson)
+}
+
+const remove = (i) => {
+  selectedLessons.value = selectedLessons.value.filter(function(value, index, arr){
+    return index !== i;
+  });
 }
 
 const save = () => {
@@ -60,6 +72,28 @@ const save = () => {
   }
 
   .btnSave {
+  }
+
+  .smallButton {
+    height: 22px;
+    margin-right: 6px;
+    cursor: pointer;
+  }
+
+  .lesson {
+    display: flex;
+    align-items: center;
+  }
+
+  .lessons {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    .lesson {
+      display: flex;
+      align-items: center;
+    }
   }
 }
 </style>
--- frontend/src/features/repo/repoLesson.js
+++ frontend/src/features/repo/repoLesson.js
@@ -1,6 +1,7 @@
 import {repoTest} from "@/features/repo/repoTest.js";
 import {repoMaterial} from "@/features/repo/repoMaterial.js";
 import {getRandomInt, shuffle} from "@/features/repo/utils.js";
+import {faker} from "@faker-js/faker";
 
 const data = []
 let materials = repoMaterial().getAll();
@@ -8,7 +9,7 @@ let tests = repoTest().getAll();
 
 for (let i = 0; i < 10 + getRandomInt(20); i++) {
   data.push({
-    title: 'Урок первый',
+    title: faker.lorem.sentence(),
     materials: materials.slice(0, 4),
     tests: tests.slice(0, 1),
   });
