d6c5a70a22405e57c2ef3ee1d79a4e6491641404
Ed Khalturin 'DVegasa <dracveg@gmail.com>
2023-02-18 10:26:56 +0300

    swiper.js

--- frontend/src/features/mobile/roadmap/PageMobileRoadmap.vue
+++ frontend/src/features/mobile/roadmap/PageMobileRoadmap.vue
@@ -1,11 +1,57 @@
 <template>
-  <div id="pageMobileRoadmap">
-    pageMobileRoadmap
+  <div class="_mp" style="padding-left: 0; padding-right: 0" id="pageMobileRoadmap">
+    <h1 style="margin-bottom: 0; padding-left: 12px">Твой план</h1>
+    <div class="caption">
+
+    </div>
+
+    <swiper
+        :effect="'coverflow'"
+        :grabCursor="true"
+        :centeredSlides="true"
+        :slidesPerView="'auto'"
+        :coverflowEffect="{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }"
+        :pagination="true"
+        :modules="modules"
+        class="mySwiper"
+    >
+      <swiper-slide class="slide" v-for="slide in slides" :key="slide.title">
+<!--        <div class="slide">-->
+          {{slide.title}}
+<!--        </div>-->
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { EffectCoverflow, Pagination } from "swiper";
+import 'swiper/css';
+
+const modules = [EffectCoverflow, Pagination];
+
+const slides = [
+  {
+    title: 'Макака',
+  },
+  {
+    title: 'Поми',
+  },
+  {
+    title: 'ВЫФА',
+  },
+  {
+    title: 'ыва',
+  },
+]
 </script>
 
 
@@ -13,6 +59,23 @@
 @import "@/styles.scss";
 
 #pageMobileRoadmap {
+  .swiper {
+    padding: 20px;
+  }
 
+  .slide {
+    height: 300px;
+    width: 75vw;
+    background-color: white;
+    border-radius: $borra;
+    border: 1px solid $color-grey;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    -webkit-box-shadow: 0px 0px 19px 0px rgba(34, 60, 80, 0.2);
+    -moz-box-shadow: 0px 0px 19px 0px rgba(34, 60, 80, 0.2);
+    box-shadow: 0px 0px 19px 0px rgba(34, 60, 80, 0.2);
+  }
 }
 </style>
