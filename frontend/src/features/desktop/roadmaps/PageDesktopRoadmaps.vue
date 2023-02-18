0a85b65251c6159346bdbed9ebcf7f39e63104f2
Ed Khalturin 'DVegasa <dracveg@gmail.com>
2023-02-18 23:41:39 +0300

    Роадмап: отображение

--- frontend/src/features/desktop/roadmaps/PageDesktopRoadmaps.vue
+++ frontend/src/features/desktop/roadmaps/PageDesktopRoadmaps.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="_dp" id="pageMobileRoadmaps">
-    <h1>Роадмап обучения</h1>
+    <h1>
+      Роадмап обучения
+    </h1>
+
+    <button class="appButton primary btnNew">Создать новый</button>
+
+    <div class="roadmaps">
+      <RoadmapInline v-for="i in 5" class="roadmap" />
+    </div>
   </div>
 </template>
 
 
 <script setup>
+import RoadmapInline from "@/features/desktop/roadmaps/RoadmapInline.vue";
 </script>
 
 
@@ -13,6 +22,14 @@
 @import "@/styles.scss";
 
 #pageMobileRoadmaps {
+  .btnNew {
+    margin-bottom: 24px;
+  }
 
+  .roadmaps {
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+  }
 }
 </style>
---
+++ frontend/src/features/desktop/roadmaps/RoadmapInline.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="roadmapInline">
+    <div class="title">Инженер 1 разряда</div>
+    <div class="bottom">
+      <div>12 заданий</div>
+      <div>•</div>
+      <div>3 чел проходят сейчас</div>
+      <div>•</div>
+      <div>73 чел прошли раньше</div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/styles.scss";
+
+#roadmapInline {
+  .title {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    opacity: 0.8;
+  }
+}
+</style>
